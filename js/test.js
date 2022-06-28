b909dfce92c594fc04b83cdeba6d5290d630bc1e
s10614165 <kweifon1998@gmail.com>
2022-06-28 09:07:27 +0000

    new add

--- js/test.js
+++ js/test.js
@@ -1,18 +1,18 @@
 const { default: axios } = require("axios");
 
-const optionmeanu= document.querySelector(".SelectMenu");
-const selectbtn=optionmeanu.querySelector(".selectbtn");
-const options=optionmeanu.querySelectorAll(".option");
-const sbtntext=optionmeanu.querySelector(".sbtntext");
-selectbtn.addEventListener("click",() => optionmeanu.classList.toggle("active"));
+const optionmeanu = document.querySelector(".SelectMenu");
+const selectbtn = optionmeanu.querySelector(".selectbtn");
+const options = optionmeanu.querySelectorAll(".option");
+const sbtntext = optionmeanu.querySelector(".sbtntext");
+selectbtn.addEventListener("click", () => optionmeanu.classList.toggle("active"));
 options.forEach(option => {
-  option.addEventListener('click',()=>{
+  option.addEventListener('click', () => {
     let selectedoption = option.querySelector(".optiontext").innerText;
     console.log(selectedoption);
-    sbtntext.innerText=selectedoption;
+    sbtntext.innerText = selectedoption;
   })
-  
- });
+
+});
 
 // let www =123;
 // let rrer ="qwewqe";
